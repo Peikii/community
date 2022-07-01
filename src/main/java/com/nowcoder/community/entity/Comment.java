50c677df02f86aef24667a7b8cf791ac6c9ef13d
Peikii <peikiiwu@gmail.com>
2022-07-01 00:19:41 -0400

    Comment Add

--- src/main/java/com/nowcoder/community/entity/Comment.java
+++ src/main/java/com/nowcoder/community/entity/Comment.java
@@ -6,6 +6,7 @@ public class Comment {
     private int id;
     private int userId;
     private int entityType;
+    private int entityId;
     private int targetId;
     private String content;
     private int status;
@@ -33,6 +34,14 @@ public class Comment {
 
     public void setEntityType(int entityType) {
         this.entityType = entityType;
+    }
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
     }
 
     public int getTargetId() {
@@ -73,6 +82,7 @@ public class Comment {
                 "id=" + id +
                 ", userId=" + userId +
                 ", entityType=" + entityType +
+                ", entityId=" + entityId +
                 ", targetId=" + targetId +
                 ", content='" + content + '\'' +
                 ", status=" + status +
